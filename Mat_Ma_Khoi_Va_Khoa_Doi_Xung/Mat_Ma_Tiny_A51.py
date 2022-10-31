877bf899b69742c24d872a35d58736f23502a0ed
longnhat9 <longnhatstar2002@gmail.com>
2022-10-31 15:18:40 +0700

    upload

--- Mat_Ma_Khoi_Va_Khoa_Doi_Xung/Mat_Ma_Tiny_A51.py
+++ Mat_Ma_Khoi_Va_Khoa_Doi_Xung/Mat_Ma_Tiny_A51.py
@@ -21,7 +21,28 @@ def maj(x, y, z) :
   else :
     return 1
 
-def quay_X_or_Y_or_Z(arr, t) :
+s = []
+
+def quay_X(arr, arr_of_x2_x4_x5) :
+  t = phep_tinh_xor(arr_of_x2_x4_x5)
+  i = len(arr) - 1
+  while i > 0 :
+    arr[i] = arr[i - 1]
+    i = i - 1
+  arr[0] = t
+  return arr
+
+def quay_Y(arr, arr_of_x6_x7) :
+  t = phep_tinh_xor(arr_of_x6_x7)
+  i = len(arr) - 1
+  while i > 0 :
+    arr[i] = arr[i - 1]
+    i = i - 1
+  arr[0] = t
+  return arr
+
+def quay_Z(arr, arr_of_x2_x7_x8) :
+  t = phep_tinh_xor(arr_of_x2_x7_x8)
   i = len(arr) - 1
   while i > 0 :
     arr[i] = arr[i - 1]
@@ -56,47 +77,21 @@ t_Z = phep_tinh_xor(arr_so_quay_z)
 
 s = []
 
-# Bước 0
-arr_tinh_t = [x[2], x[4], x[5]]
-t_X = phep_tinh_xor(arr_tinh_t)
-m = maj(x[1], y[3], z[3])
+for i in range(0, len(ban_ro)) :
+  arr_tinh_t_X = [x[2], x[4], x[5]]
+  arr_tinh_t_Y = [y[6], y[7]]
+  arr_tinh_t_Z = [z[2], z[7], z[8]]
+  m = maj(x[1], y[3], z[3])
 
-if x[1] == m :
-  x = quay_X_or_Y_or_Z(x, t_X)
-if y[3] == m :
-  y = quay_X_or_Y_or_Z(y, t_Y)
-if z[3] == m :
-  z = quay_X_or_Y_or_Z(z, t_Z)
-
-s.append(phep_tinh_xor(arr_tinh_t))
-
-# Bước 1
-arr_tinh_t = [y[6], y[7]]
-t_Y = phep_tinh_xor(arr_tinh_t)
-m = maj(x[1], y[3], z[3])
-
-if x[1] == m :
-  x = quay_X_or_Y_or_Z(x, t_X)
-if y[3] == m :
-  y = quay_X_or_Y_or_Z(y, t_Y)
-if z[3] == m :
-  z = quay_X_or_Y_or_Z(z, t_Z)
-
-s.append(phep_tinh_xor(arr_tinh_t))
-
-# Bước 2
-arr_tinh_t = [z[2], z[7], z[8]]
-t_Z = phep_tinh_xor(arr_tinh_t)
-m = maj(x[1], y[3], z[3])
-
-if x[1] == m :
-  x = quay_X_or_Y_or_Z(x, t_X)
-if y[3] == m :
-  y = quay_X_or_Y_or_Z(y, t_Y)
-if z[3] == m :
-  z = quay_X_or_Y_or_Z(z, t_Z)
-
-s.append(phep_tinh_xor(arr_tinh_t))
+  if x[1] == m :
+    x = quay_X(x, arr_tinh_t_X)
+  if y[3] == m :
+    y = quay_Y(y, arr_tinh_t_Y)
+  if z[3] == m :
+    z = quay_Z(z, arr_tinh_t_Z)
+  
+  arr_tinh_s = [x[5], y[7], z[8]]
+  s.append(phep_tinh_xor(arr_tinh_s))
 
 arr_c = []
 
@@ -117,6 +112,7 @@ while i >= 0 :
 
 str_ky_tu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+arr_ky_tu = []
 for i in range(0, len(str_ky_tu)) :
   if i == sum :
     print(str_ky_tu[i])
